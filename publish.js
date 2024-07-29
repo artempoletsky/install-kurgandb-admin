f1964fcfccbaf299ea8fee5d5e8c4ddd2c044c76
Artem Poletsky <artempoletsky@gmail.com>
2024-07-29 10:52:26 +0500

    fix

--- publish.js
+++ publish.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 
 const CWD = process.cwd();
 
+fs.copyFileSync(CWD + "/package.json", CWD + "/package.installed.json");
 fs.copyFileSync(CWD + "/package.npm.json", CWD + "/package.json");
 
 const lockFile = CWD + "/package-lock.json";
